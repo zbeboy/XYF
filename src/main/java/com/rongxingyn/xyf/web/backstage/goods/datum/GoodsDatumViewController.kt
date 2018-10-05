21e30d024c49d9a3ac03775b3882e84a971c7dfc
zbeboy <863052317@qq.com>
2018-10-05 19:08:23 +0800

    优化

--- src/main/java/com/rongxingyn/xyf/web/backstage/goods/datum/GoodsDatumViewController.kt
+++ src/main/java/com/rongxingyn/xyf/web/backstage/goods/datum/GoodsDatumViewController.kt
@@ -5,7 +5,6 @@ import com.rongxingyn.xyf.service.backstage.goods.datum.GoodsDatumService
 import com.rongxingyn.xyf.service.backstage.goods.datum.GoodsPicsService
 import org.springframework.stereotype.Controller
 import org.springframework.ui.Model
-import org.springframework.ui.ModelMap
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RequestMapping
