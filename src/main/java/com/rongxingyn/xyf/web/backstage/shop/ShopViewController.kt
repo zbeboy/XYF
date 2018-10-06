66af5a0d2c98d1e28e8e80f578e1858d85d8c3a5
zbeboy <863052317@qq.com>
2018-10-06 14:05:49 +0800

    网站资料完成

--- src/main/java/com/rongxingyn/xyf/web/backstage/shop/ShopViewController.kt
+++ src/main/java/com/rongxingyn/xyf/web/backstage/shop/ShopViewController.kt
@@ -1,13 +1,20 @@
 package com.rongxingyn.xyf.web.backstage.shop
 
+import com.rongxingyn.xyf.service.backstage.data.DataInfoService
+import com.rongxingyn.xyf.service.backstage.data.DataKey
 import org.springframework.stereotype.Controller
+import org.springframework.ui.Model
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import reactor.core.publisher.Mono
+import javax.annotation.Resource
 
 @Controller
 @RequestMapping("/web/backstage/shop")
 open class ShopViewController {
+
+    @Resource
+    open lateinit var dataInfoService: DataInfoService
 
     /**
      * 店铺资料页
@@ -15,7 +22,13 @@ open class ShopViewController {
      * @return list page
      */
     @GetMapping("/list")
-    fun list(): Mono<String> {
+    fun list(model: Model): Mono<String> {
+        val record = dataInfoService.findByPrefix(DataKey.SHOP.name)
+        if (record.isNotEmpty) {
+            record.forEach { r ->
+                model.addAttribute(r.dataKey, r.dataValue)
+            }
+        }
         return Mono.just("backstage/shop/shop_list")
     }
 }
--- src/main/java/com/rongxingyn/xyf/web/backstage/website/WebsiteViewController.kt
+++ src/main/java/com/rongxingyn/xyf/web/backstage/website/WebsiteViewController.kt
@@ -1,13 +1,20 @@
 package com.rongxingyn.xyf.web.backstage.website
 
+import com.rongxingyn.xyf.service.backstage.data.DataInfoService
+import com.rongxingyn.xyf.service.backstage.data.DataKey
 import org.springframework.stereotype.Controller
+import org.springframework.ui.Model
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import reactor.core.publisher.Mono
+import javax.annotation.Resource
 
 @Controller
 @RequestMapping("/web/backstage/website")
 open class WebsiteViewController {
+
+    @Resource
+    open lateinit var dataInfoService: DataInfoService
 
     /**
      * 网站设置页
@@ -15,7 +22,13 @@ open class WebsiteViewController {
      * @return list page
      */
     @GetMapping("/list")
-    fun list(): Mono<String> {
+    fun list(model: Model): Mono<String> {
+        val record = dataInfoService.findByPrefix(DataKey.WEBSITE.name)
+        if (record.isNotEmpty) {
+            record.forEach { r ->
+                model.addAttribute(r.dataKey, r.dataValue)
+            }
+        }
         return Mono.just("backstage/website/website_list")
     }
 }
